4b81e5838c6028bd43f75a48d537127000f4d541
Nick Botelho <nickpbotelho@gmail.com>
2021-04-29 19:41:50 -0400

    added error handle to when server returns game with no players

--- getGames.py
+++ getGames.py
@@ -1,7 +1,7 @@
 def getGames(info):
     res = "No Games :(" if len(info) == 0 else "Current Games:\n"
     for game in info:
-        if len(game['players'] > 0):
+        if len(game['players']) > 0:
             host = game['players'][0]['name'] #grab host name
             host+= "'s"
             res+= "{}, Occupancy: ({}/{})\n".format(host, game['playerCount'], game['maxPlayers'])
