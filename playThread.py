d52b906837765ad8e63aed60325f5546d505b16c
Nick Botelho <nickpbotelho@gmail.com>
2021-12-07 23:35:53 -0500

    changed timezone to ET

--- playThread.py
+++ playThread.py
@@ -1,5 +1,9 @@
 import discord
 from time import strftime, localtime
+import time
+import os
+os.environ['TZ'] = 'EST+05EDT,M4.1.0,M10.5.0'
+time.tzset()
 def updatePlayEmbed(stack):
     chat_embed = discord.Embed(
         title = 'Players Who Want To Play',
